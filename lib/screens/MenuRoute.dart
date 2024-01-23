917a32e6104a97bd70e674def1e724ac3ae1ff85
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-01-23 21:08:44 +0300

    Initial commit

---
+++ lib/screens/MenuRoute.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MenuRoute extends StatelessWidget {
+  const MenuRoute();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Screen"),
+      ),
+      body: ListView(
+        children: [],
+      ),
+    );
+  }
+}
